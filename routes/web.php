57f46dcfc29073979e9c6c123a2033d93cb9bc7d
RusulAzomSumon <18103020@iubat.edu>
2023-11-07 19:32:36 +0600

    template conecting done

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Frontend\FrontendController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// retun blog home 
+Route::get('/', [FrontendController::class, 'index'])->name('front.index');
