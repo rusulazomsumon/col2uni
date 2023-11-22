10bf56378033a0efe249cac299c83bd39127e033
RusulAzomSumon <18103020@iubat.edu>
2023-11-22 20:00:21 +0600

    registration blocked

--- resources/views/backend/auth/register.blade.php
+++ resources/views/backend/auth/register.blade.php
@@ -2,26 +2,19 @@
 @section('page_title','Register Form')
 
 @section('content')
-    {!! Form::open(['method'=>'post', 'route'=>'register']) !!}
-    <!-- name -->
+    {{-- {!! Form::open(['method'=>'post', 'route'=>'register']) !!}
     {!! Form::label('name', 'Name') !!}
     {!! Form::text('name', null, ['class'=>'form-control from-control-sm']) !!}
-     
-    <!-- email -->
     {!! Form::label('email', 'Email') !!}
     {!! Form::email('email', null, ['class'=>'form-control from-control-sm']) !!}
-     <!-- password -->
-     {!! Form::label('password', 'Password') !!}
-     {!! Form::password('password'  , ['class'=>'form-control from-control-sm']) !!}
-     <!-- password confromation -->
-     {!! Form::label('password_confirmation', 'Password Confirmation') !!}
-     {!! Form::password('password_confirmation', ['class'=>'form-control from-control-sm']) !!}
-    {{-- submin button --}}
-     <div class="d-grid">
-        {!! Form::button('Sign Up', ['type'=>'submit','class'=>'btn btn-info btn-sm mt-2']) !!}
+    {!! Form::label('password', 'Password') !!}
+    {!! Form::password('password'  , ['class'=>'form-control from-control-sm']) !!}
+    {!! Form::label('password_confirmation', 'Password Confirmation') !!}
+    {!! Form::password('password_confirmation', ['class'=>'form-control from-control-sm']) !!}
+    <div class="d-grid">
+        {!! Form::button('Sign Up', ['type'=>'submit','class'=>'btn btn-info btn-sm mt-2', 'disabled' => true]) !!}
     </div>
-    {{-- end of form --}}
-    {!! Form::close([]) !!}
+    {!! Form::close([]) !!} --}}
 
     {{-- Sign in --}}
     <p class="mt-2">
